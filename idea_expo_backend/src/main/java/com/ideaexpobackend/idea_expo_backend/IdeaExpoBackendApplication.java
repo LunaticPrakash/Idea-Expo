4ac7b76a123cfce69d3e7fba886e2c950151efb5
Prakash Gupta <prakash.25gupta@gmail.com>
2024-10-19 09:34:57 +0530

    Added backend boilterplate code

---
+++ idea_expo_backend/src/main/java/com/ideaexpobackend/idea_expo_backend/IdeaExpoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ideaexpobackend.idea_expo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaExpoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaExpoBackendApplication.class, args);
+	}
+
+}
---
+++ idea_expo_backend/src/test/java/com/ideaexpobackend/idea_expo_backend/IdeaExpoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideaexpobackend.idea_expo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaExpoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
