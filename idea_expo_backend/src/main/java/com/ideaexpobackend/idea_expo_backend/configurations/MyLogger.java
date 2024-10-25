933b5a664c7fa000f1093c5b646740d16a1eed31
Prakash Gupta <prakash.25gupta@gmail.com>
2024-10-25 18:08:54 +0530

    Added custom logger functionality for org.hibernate & com.ideaexpobackend package.

---
+++ idea_expo_backend/src/main/java/com/ideaexpobackend/idea_expo_backend/configurations/MyLogger.java
@@ -0,0 +1,91 @@
+package com.ideaexpobackend.idea_expo_backend.configurations;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.ConsoleAppender;
+import ch.qos.logback.core.rolling.RollingFileAppender;
+import ch.qos.logback.core.rolling.SizeAndTimeBasedRollingPolicy;
+import ch.qos.logback.core.util.FileSize;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class MyLogger {
+
+    // Static constants for log configuration
+    private static final String HIBERNATE_LOGGER = "org.hibernate";
+    private static final String MYAPP_LOGGER = "com.ideaexpobackend";
+    private static final String HIBERNATE_LOG_PATH = "Logs/HibernateLogs.log";
+    private static final String MYAPP_LOG_PATH = "Logs/IdeaExpoLogs.log";
+    private static final String FILE_LOG_PATTERN = "%d{dd-MMM-yyyy HH:mm:ss} || [%thread] || %-5level || %logger{36} || %file || %C{0} || %method || %line || %msg |||%n";
+    private static final String CONSOLE_LOG_PATTERN = "%d{dd-MMM-yyyy HH:mm:ss} - [%thread] - %-5level - %logger{36} - %file - %line - %msg%n";
+
+
+    @Bean
+    public String configureCustomLoggers() {
+        configureCustomLogger(MYAPP_LOGGER, Level.DEBUG, CONSOLE_LOG_PATTERN, FILE_LOG_PATTERN, MYAPP_LOG_PATH);
+        configureCustomLogger(HIBERNATE_LOGGER, Level.DEBUG, CONSOLE_LOG_PATTERN, FILE_LOG_PATTERN, HIBERNATE_LOG_PATH);
+        return "Loggers Configured";
+    }
+
+    public void configureCustomLogger(String loggerName, Level logLevel, String consolePattern, String filePattern, String filePath) {
+        LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+
+        ConsoleAppender<ILoggingEvent> consoleAppender = createConsoleAppender(context, consolePattern);
+        RollingFileAppender<ILoggingEvent> fileAppender = createFileAppender(context, filePattern, filePath);
+
+        Logger logger = (Logger) LoggerFactory.getLogger(loggerName);
+        logger.setLevel(logLevel);
+        logger.addAppender(consoleAppender);
+        logger.addAppender(fileAppender);
+
+        if(!loggerName.equals(Logger.ROOT_LOGGER_NAME))
+            logger.setAdditive(false); // Prevent logging to root logger
+    }
+
+    private ConsoleAppender<ILoggingEvent> createConsoleAppender(LoggerContext context, String pattern) {
+        ConsoleAppender<ILoggingEvent> consoleAppender = new ConsoleAppender<>();
+        consoleAppender.setContext(context);
+        consoleAppender.setEncoder(createEncoder(context, pattern));
+        consoleAppender.start();
+        return consoleAppender;
+    }
+
+    private RollingFileAppender<ILoggingEvent> createFileAppender(LoggerContext context, String pattern, String filePath) {
+        RollingFileAppender<ILoggingEvent> fileAppender = new RollingFileAppender<>();
+        fileAppender.setContext(context);
+        fileAppender.setFile(filePath);
+
+        SizeAndTimeBasedRollingPolicy<ILoggingEvent> rollingPolicy = createRollingPolicy(context, fileAppender, filePath);
+        fileAppender.setRollingPolicy(rollingPolicy);
+
+        fileAppender.setEncoder(createEncoder(context, pattern));
+        fileAppender.start();
+        return fileAppender;
+    }
+
+    private PatternLayoutEncoder createEncoder(LoggerContext context, String pattern) {
+        PatternLayoutEncoder encoder = new PatternLayoutEncoder();
+        encoder.setContext(context);
+        encoder.setPattern(pattern);
+        encoder.start();
+        return encoder;
+    }
+
+    private SizeAndTimeBasedRollingPolicy<ILoggingEvent> createRollingPolicy(LoggerContext context, RollingFileAppender<ILoggingEvent> fileAppender, String filePath) {
+        SizeAndTimeBasedRollingPolicy<ILoggingEvent> rollingPolicy = new SizeAndTimeBasedRollingPolicy<>();
+        rollingPolicy.setContext(context);
+        rollingPolicy.setParent(fileAppender);
+        rollingPolicy.setFileNamePattern(filePath + "_%d{dd-MMM-yyyy}_%i.log.gz");
+        rollingPolicy.setMaxFileSize(FileSize.valueOf("100MB"));
+        rollingPolicy.setMaxHistory(30);
+        rollingPolicy.setTotalSizeCap(FileSize.valueOf("5GB"));
+        rollingPolicy.start();
+        return rollingPolicy;
+    }
+}
