1bcfea7b5c31356d8dde2a97b8d4f9b990d7a315
Prakash Gupta <prakash.25gupta@gmail.com>
2025-07-02 22:27:22 +0530

    Set configuration in the app

--- idea_expo_backend/app/__init__.py
+++ idea_expo_backend/app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from .config import get_config
 from .routes import register_routes
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(get_config(env="DEV"))
     register_routes(app)
     return app
