a37a1f95e508ebfe6ce808a8dcb30be3e8f03317
Prakash Gupta <prakash.25gupta@gmail.com>
2025-07-06 16:26:42 +0530

    Added register user

--- idea_expo_backend/app/db/models/user.py
+++ idea_expo_backend/app/db/models/user.py
@@ -1,4 +1,5 @@
 from .. import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer,primary_key=True)
@@ -6,4 +7,16 @@ class User(db.Model):
     last_name = db.Column(db.String(50))
     email = db.Column(db.String(80), nullable=False, unique = True)
     password = db.Column(db.String(80), nullable=False)
+
+    def __init__(self, first_name, last_name, email, password) -> None:
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = self.set_password(password)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
     
--- idea_expo_backend/app/routes/auth_routes.py
+++ idea_expo_backend/app/routes/auth_routes.py
@@ -1,11 +1,23 @@
 from flask import request, jsonify
-
+from ..db import db
+from ..db.models import User
 def register_auth_routes(app):
     
     @app.route(rule="/api/auth/register",methods=["POST"])
     def register_user():
         req_body = request.json
-        return jsonify(req_body)
+        first_name = req_body.get('first_name')
+        last_name = req_body.get('last_name')
+        email = req_body.get('email')
+        password = req_body.get('password')
+
+        if User.query.filter_by(email=email).first():
+            return jsonify({"error": "Email already registered"}), 409
+        user = User(first_name, last_name, email, password)
+        db.session.add(user)
+        db.session.commit()
+
+        return jsonify({"message": "User registered successfully"}), 201
 
     @app.route(rule="/api/auth/login",methods=["POST"])
     def login_user():
