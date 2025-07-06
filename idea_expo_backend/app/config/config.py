666676f12a06fd3c9fac7eb976ba21f63f4fed91
Prakash Gupta <prakash.25gupta@gmail.com>
2025-07-06 16:25:58 +0530

    Added BaseConfig and RBAC configs

--- idea_expo_backend/app/config/config.py
+++ idea_expo_backend/app/config/config.py
@@ -1,16 +1,28 @@
-class DEVConfig:
+class BaseConfig:
 
     # Database
     SQLALCHEMY_DATABASE_URI = "sqlite:///db.sqlite3"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-class QAConfig:
+    # Role based authorization
+    RBAC_RULES = {
+    '/api/admin/*': ['admin'],
+    '/api/user/': ['user', 'admin'],
+}
+
+class DEVConfig(BaseConfig):
 
     # Database
     SQLALCHEMY_DATABASE_URI = "sqlite:///db.sqlite3"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-class PRODConfig:
+class QAConfig(BaseConfig):
+
+    # Database
+    SQLALCHEMY_DATABASE_URI = "sqlite:///db.sqlite3"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class PRODConfig(BaseConfig):
 
     # Database
     SQLALCHEMY_DATABASE_URI = "sqlite:///db.sqlite3"
